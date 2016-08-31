3b76e3af4d9ce4e76104afddbdef32e5e6a20e4b
jhamer02 <jhamer02@hotmail.com>
2016-08-31 18:52:36 -0500

    Initial Commit

---
+++ Conekta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Conekta
+//
+//  Created by Jorge Jhamil Pineda Echeverria on 31/08/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Conekta/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Conekta
+//
+//  Created by Jorge Jhamil Pineda Echeverria on 31/08/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
